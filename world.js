e1d2aa3ec26afcc0a44f80c70ff0f601203bb0c8
AundreE <32345683+AundreE@users.noreply.github.com>
2017-11-14 16:06:36 -0600

    Exercise 4

--- world.js
+++ world.js
@@ -22,7 +22,6 @@ $(document).ready(function()
             alert(data); 
             $("#result").append(data);
          }).fail(function()
-         
          {
              alert("ERROR!");
          });
@@ -30,5 +29,16 @@ $(document).ready(function()
      }
      
      
- }); 
+ });
  
+ function searchAll(){
+     var url = 'https://lab7-riddimboss.c9users.io/world.php?all=true';
+     
+     $.ajax(url,{
+         method: 'GET',
+     }).done(function(res){
+         $("#result").html(res);
+     }).fail(function(fail){
+         $("#result").html('<b>AJAX ERROR</b>');
+     });
+  }
--- world.php
+++ world.php
@@ -1,19 +1,36 @@
 <?php
-
-$host = getenv('IP');
-$username = getenv('C9_USER');
-$password = '';
-$dbname = 'world';
-$country= $_GET['country'];
-
-
-$conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-
-$stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
-
-$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-echo '<ul>';
-foreach ($results as $row) {
-  echo '<li>' . $row['name'] . ' is ruled by ' . $row['head_of_state'] . '</li>';
-}
-echo '</ul>';
+ 
+ $host = getenv('IP');
+ $username = getenv('C9_USER');
+ $password = '';
+  $dbname = 'world';
+  
+  $country = $_GET['country'];
+ $all = $_GET['all'];
+  
+  $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+  
+ if($all =='true'){
+     $stmt = $conn->query("SELECT * FROM countries");
+  
+     $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+  
+     echo '<ul>';
+     foreach ($results as $row) {
+       echo '<li>' . $row['name'] . ' is ruled by ' . $row['head_of_state'] . '</li>';
+     }
+     echo '</ul>';
+     
+  }
+ 
+ else if ($country !== ""){
+     $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%';");
+ 
+     $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+ 
+     echo '<ul>';
+     foreach ($results as $row) {
+       echo '<li>' . $row['name'] . ' is ruled by ' . $row['head_of_state'] . '</li>';
+     }
+     echo '</ul>';
+ }
